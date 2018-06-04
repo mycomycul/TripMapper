b7d7ca2bb87b48034383e06275bb0f155a3d0f79
Michael Strunk <mycomycul@gmail.com>
2018-06-03 20:49:10 -0700

    Added Quill

---
+++ scripts/quill.js
@@ -0,0 +1,7 @@
+var options = {
+    debug: 'info',
+    placeholder: 'Compose an epic...',
+    theme: 'snow'
+    }
+
+var editor = new Quill('#textTarget',options);
--- scripts/text.js
+++ scripts/text.js
@@ -1,18 +1,47 @@
-document.getElementById('textTarget').onkeydown = function(){saveTextStatus(this)};
-function saveTextStatus(textTarget) {
-    getTextFromElements(textTarget);
-   // var textSample = document.getElementById('textSample');
-   // textSample.innerHTML = textTarget.innerHTML + "turkey";
+var textTarget = document.getElementById('textTarget');
+var textSample = document.getElementById('textSample');
+var textToHTML = document.getElementById('textToHTML');
+textTarget.onkeydown = function(){textTest()};
+
+
+function getTextContent(textTarget) {
+   
+   textSample.innerHTML = textTarget.textContent;
 }
+
 
 function getTextFromElements(fullHTML) {
     var nodeList = fullHTML.querySelectorAll('*');
-    var textSample = document.getElementById('textSample');                             for(var i = 0; i < nodeList.length; i++){
+                                 for(var i = 0; i < nodeList.length; i++){
         textSample.innerHTML += nodeList[i].innerHTML;
     }      
 }
 
-function saveSelection(textSelection) {
-    var textSample = document.getElementById('textSample');
-    textSample.innerHTML = textSelection.innerHTML + textSelection.;
+
+function getSelectionFromElements(textSelection) {
+    
+    var s = window.getSelection();
+    textSample.innerHTML = s;
+}
+
+//
+function iterateNodes(nodeObject){
+    for(var i = 0; i < nodeObject.childNodes.length; i++){
+        if (nodeObject.childNodes[i].childNodes.length >0){
+            iterateNodes(nodeObject.childNodes[i]);
+        }
+        else{
+            nodeValue(nodeObject.childNodes[i]);
+        }
+    }
+}
+function nodeValue(nodeObject){
+        textSample.innerHTML += nodeObject.nodeValue;
+}
+function getinnerHTML(){
+    textSample.innerHTML = textTarget.innerHTML;
+}
+
+function textTest(){
+   textToHTML.innerHTML = editor.root.innerHTML; 
 }
