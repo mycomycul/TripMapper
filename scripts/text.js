ecc1e699d06da2771bda8975a1596f51ba1cb47e
Michael Strunk <mycomycul@gmail.com>
2018-08-13 07:55:57 -0700

    Delete quill and replace with content

--- scripts/text.js
+++ scripts/text.js
@@ -10,7 +10,14 @@ function newSection(){
 var newSection = document.createElement("section");
 newSection.setAttribute("id",Math.random())
 newSection.innerHTML = editor.root.innerHTML;
-var target = document.getElementById('QuillTarget').parentElement;
-target.insertBefore(newSection,target.querySelector('.ql-toolbar'));
-editor.root.innerHTML = ""
+// var target = document.getElementById('QuillTarget').parentElement;
+// target.insertBefore(newSection,target.querySelector('.ql-toolbar'));
+// editor.root.innerHTML = "";
+
+var element = document.getElementById('QuillTarget').parentNode;
+while (element.lastChild){
+    element.removeChild(element.lastChild);
+}
+element.appendChild(newSection)
+
 }
